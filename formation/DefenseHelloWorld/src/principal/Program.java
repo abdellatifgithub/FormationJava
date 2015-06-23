bf9abde95409b3c442de771cb342a3c99dcc2352
abdellatifgithub <abdellatif.mba@gmail.com>
2015-06-23 12:39:03 +0200

    bonne app

--- formation/DefenseHelloWorld/src/principal/Program.java
+++ formation/DefenseHelloWorld/src/principal/Program.java
@@ -3,7 +3,8 @@ package principal;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("bonjour monde");
+		System.out.println("Bonjour monde");
+		System.out.println("et bonne appetit");
 
 	}
 
